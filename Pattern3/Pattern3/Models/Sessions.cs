9cdf7d2b0b53ad9044c26f3aed87a64774eae175
IHermanovych <IVAN.HERMANOVYCH@lnu.edu.ua>
2025-12-11 18:16:34 +0100

    Add files via upload

---
+++ Pattern3/Pattern3/Models/Sessions.cs
@@ -0,0 +1,33 @@
+﻿using Scheduling.Models.Teachers;
+using System;
+
+namespace Scheduling.Models.Sessions
+{
+    public abstract class ClassSession
+    {
+        public string Time { get; }
+        public string Room { get; }
+        public Teacher Teacher { get; }
+
+        protected ClassSession(string time, string room, Teacher teacher)
+        {
+            Time = time ?? throw new ArgumentNullException(nameof(time));
+            Room = room ?? throw new ArgumentNullException(nameof(room));
+            Teacher = teacher ?? throw new ArgumentNullException(nameof(teacher));
+        }
+
+        public virtual string Describe() => $"{GetType().Name} at {Time} in {Room} by {Teacher}";
+    }
+
+    public class LectureSession : ClassSession
+    {
+        public LectureSession(string time, string room, ICanGiveLecture teacher)
+            : base(time, room, teacher as Teacher) { }
+    }
+
+    public class PracticalSession : ClassSession
+    {
+        public PracticalSession(string time, string room, ICanLeadPractical teacher)
+            : base(time, room, teacher as Teacher) { }
+    }
+}
---
+++ Pattern3/Pattern3/Models/Teachers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Scheduling.Models.Teachers
+{
+    public abstract class Teacher
+    {
+        public string Name { get; }
+        protected Teacher(string name) => Name = name ?? throw new ArgumentNullException(nameof(name));
+        public override string ToString() => $"{GetType().Name} {Name}";
+    }
+
+    public interface ICanGiveLecture { }
+    public interface ICanLeadPractical { }
+    public interface ICanSuperviseCourseWork { }
+
+    public class Lecturer : Teacher, ICanGiveLecture, ICanSuperviseCourseWork
+    {
+        public Lecturer(string name) : base(name) { }
+    }
+
+    public class Assistant : Teacher, ICanLeadPractical, ICanSuperviseCourseWork
+    {
+        public Assistant(string name) : base(name) { }
+    }
+
+    public class ExternalMentor : Teacher, ICanSuperviseCourseWork
+    {
+        public ExternalMentor(string name) : base(name) { }
+    }
+}
